3f561502faa2f786809b31455ac7002ec40d91d5
wzj <wzj@25e0eac6-5c60-e44f-9c6f-99974a5b1bee>
2019-05-18 17:06:20 +0000

    git-svn-id: https://39.108.144.9:8443/svn/education_game@18 25e0eac6-5c60-e44f-9c6f-99974a5b1bee

--- app/Http/Controllers/Pc/LevelOneController.php
+++ app/Http/Controllers/Pc/LevelOneController.php
@@ -6,6 +6,8 @@ use Route,Auth;
 use Illuminate\Http\Request;
 use App\Models\LevelOneVideo;
 use App\Models\User;
+use App\Models\UserAnswer;
+use App\Models\Question;
 use App\Models\UserLevelOneVideoTolerateGrade;
 use App\Models\UserLevelOneVideoCategory;
 use App\Models\UserLevelOneVideoCategoryNoticeGrade;
@@ -40,6 +42,37 @@ class LevelOneController extends BaseController
 
         $user_grade = UserLevelOneVideoTolerateGrade::where('user_id',$user_id)->where('video_id',$video->id)->first();
 
+        $last_level_question = Question::where('level_id',1)->orderBy('id','desc')->first();
+
+        $last_level_question_user_answer = UserAnswer::where('user_id',$this->user->id)->where('level_id',1)->where('question_id',$last_level_question->id)->orderBy('id','desc')->first();
+
+        if(!$last_level_question_user_answer)
+        {
+            UserLevelOneVideoTolerateGrade::create([
+                'user_id' => $user_id,
+                'video_id' => $video->id,
+                'grade' => $grade
+            ]);
+        }else{
+            $last_user_grade = UserLevelOneVideoTolerateGrade::where('user_id',$user_id)->where('video_id',$video->id)->where('created_at','>',$last_level_question_user_answer->created_at)->first();
+
+            if($last_user_grade)
+            {
+                UserLevelOneVideoTolerateGrade::where('id',$last_user_grade->id)
+                    ->update([
+                        'video_id' => $video->id,
+                        'grade' => $grade
+                    ]);
+            }else{
+                UserLevelOneVideoTolerateGrade::create([
+                    'user_id' => $user_id,
+                    'video_id' => $video->id,
+                    'grade' => $grade
+                ]);
+            }
+        }
+
+        /*
         if($user_grade)
         {
             UserLevelOneVideoTolerateGrade::where('id',$user_grade->id)
@@ -54,7 +87,7 @@ class LevelOneController extends BaseController
                 'grade' => $grade
             ]);
         }
-
+        */
         return $this->response->message(trans('messages.submit_success'))
             ->status("success")
             ->code(200)
@@ -70,9 +103,18 @@ class LevelOneController extends BaseController
         {
             $request_data = json_decode($request_data,true);
         }
-
+        $request_data = [
+            'a1' => 1,
+            'a2' => 3
+        ];
         $video_category_data = [];
         $i = 0;
+
+        $last_level_question = Question::where('level_id',1)->orderBy('id','desc')->first();
+
+        $last_level_question_user_answer = UserAnswer::where('user_id',$this->user->id)->where('level_id',1)->where('question_id',$last_level_question->id)->orderBy('id','desc')->first();
+
+
         foreach ($request_data as $key => $val)
         {
             $i++;
@@ -83,6 +125,31 @@ class LevelOneController extends BaseController
                 'category_id' => $val
             ];
             $video = LevelOneVideo::where('slug',$key)->first();
+
+            if(!$last_level_question_user_answer)
+            {
+                UserLevelOneVideoCategory::create([
+                    'user_id' => $this->user->id,
+                    'video_id' => $video->id,
+                    'category_id' => $val
+                ]);
+            }else{
+                $last_user_category = UserLevelOneVideoCategory::where('user_id',$this->user->id)->where('video_id',$video->id)->where('created_at','>',$last_level_question_user_answer->created_at)->first();
+
+                if($last_user_category)
+                {
+                    UserLevelOneVideoCategory::where('id',$last_user_category->id)->update([
+                        'category_id' => $val
+                    ]);
+                }else{
+                    UserLevelOneVideoCategory::create([
+                        'user_id' => $this->user->id,
+                        'video_id' => $video->id,
+                        'category_id' => $val
+                    ]);
+                }
+            }
+            /*
             $user_video_category = UserLevelOneVideoCategory::where('video_id',$video->id)->where('user_id',$this->user->id)->first();
             if($user_video_category)
             {
@@ -96,6 +163,7 @@ class LevelOneController extends BaseController
                     'category_id' => $val
                 ]);
             }
+            */
         }
 
         return $this->response->message(trans('messages.submit_success'))
@@ -114,6 +182,36 @@ class LevelOneController extends BaseController
 
         $user_grade = UserLevelOneVideoCategoryNoticeGrade::where('user_id',$user_id)->where('category_id',$category_id)->first();
 
+        $last_level_question = Question::where('level_id',1)->orderBy('id','desc')->first();
+
+        $last_level_question_user_answer = UserAnswer::where('user_id',$this->user->id)->where('level_id',1)->where('question_id',$last_level_question->id)->orderBy('id','desc')->first();
+
+        if(!$last_level_question_user_answer)
+        {
+            UserLevelOneVideoCategoryNoticeGrade::create([
+                'user_id' => $user_id,
+                'category_id' => $category_id,
+                'grade' => $grade
+            ]);
+        }else{
+            $last_user_grade = UserLevelOneVideoCategoryNoticeGrade::where('user_id',$user_id)->where('category_id',$category_id)->where('created_at','>',$last_level_question_user_answer->created_at)->first();
+
+            if($last_user_grade)
+            {
+                UserLevelOneVideoCategoryNoticeGrade::where('id',$last_user_grade->id)
+                    ->update([
+                        'category_id' => $category_id,
+                        'grade' => $grade
+                    ]);
+            }else{
+                UserLevelOneVideoCategoryNoticeGrade::create([
+                    'user_id' => $user_id,
+                    'category_id' => $category_id,
+                    'grade' => $grade
+                ]);
+            }
+        }
+        /*
         if($user_grade)
         {
             UserLevelOneVideoCategoryNoticeGrade::where('id',$user_grade->id)
@@ -128,7 +226,7 @@ class LevelOneController extends BaseController
                 'grade' => $grade
             ]);
         }
-
+        */
         return $this->response->message(trans('messages.submit_success'))
             ->status("success")
             ->code(200)
